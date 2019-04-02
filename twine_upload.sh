948372266cf85f44812875fa6fd37fbdf1e834e8
Brian Quinlan <brian@sweetapp.com>
2019-04-02 05:55:41 -0700

    Add a PyPI upload script for CircleCI.

---
+++ twine_upload.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 The pybadge Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ev
+
+# If this is not a CircleCI tag, no-op.
+if [[ -z "$CIRCLE_TAG" ]]; then
+  echo "This is not a release tag. Doing nothing."
+  exit 0
+fi
+
+python3 -m pip install --upgrade twine wheel setuptools
+
+# Build the distribution and upload.
+python3 setup.py sdist bdist_wheel
+twine upload dist/* --username $PYPI_USERNAME --password $PYPI_PASSWORD
