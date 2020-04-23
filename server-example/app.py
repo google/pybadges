0bd4d05dd9c4840cef93ef280d241e1e6a863a5d
AlexeyDer <hottelmich@gmail.com>
2020-04-23 11:01:43 +0700

    Add license, right comment and format code

--- server-example/app.py
+++ server-example/app.py
@@ -1,4 +1,18 @@
-# Example CI server that serves badges
+# Copyright 2018 The pybadge Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""" Example CI server that serves badges """
 
 from flask import Flask
 import pybadges
@@ -12,13 +26,11 @@ def serveBadges():
     badge_arg = dict(
         left_text='build',
         right_text='passing',
-        right_color='#008000'
-    )
+        right_color='#008000')
     badge = pybadges.badge(**badge_arg)
 
     # Second example
     secondBadge = pybadges.badge('chat', 'online')
-
     return badge + "\n" + secondBadge
 
 
