9a98b7e69613b4e58d663c3f64dd5dc306651cef
AlexeyDer <hottelmich@gmail.com>
2020-04-25 00:22:57 +0700

    Corrected description

--- server-example/app.py
+++ server-example/app.py
@@ -1,4 +1,4 @@
-# Copyright 2018 The pybadge Authors
+# Copyright 2020 The pybadge Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""" Example CI server that serves badges """
+""" Example CI server that serves badges."""
 
 from flask import Flask
 import pybadges
